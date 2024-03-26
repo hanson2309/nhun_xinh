97d4831dc8bc4d51b591a0bb02dd2662d02fbee1
Son Han <hanhongson239@gmail.com>
2024-03-27 00:18:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import React, {Component} from 'react';
 import {CKEditor} from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import './App.css'
+import {Button, Drawer, Collapse, Card, Flex} from 'antd';
+import {RollbackOutlined} from "@ant-design/icons";
+
 
 class App extends Component {
 
@@ -20,8 +23,29 @@ class App extends Component {
         kor: "",
         vie: "",
         result: "",
-        btnText: 'Copy'
+        btnText: 'Copy',
+        isHistoriesOpen: false,
+        histories: []
     }
+
+    items = [
+        {
+            key: '1',
+            label: 'This is panel header 1',
+            children: <p>a</p>,
+        },
+        {
+            key: '2',
+            label: 'This is panel header 2',
+            children: <p>s</p>,
+        },
+        {
+            key: '3',
+            label: 'This is panel header 3',
+            children: <p>v</p>,
+        },
+    ];
+
 
     mergeRight = () => {
         const blankRow = "<p>&nbsp;</p>"
@@ -51,7 +75,7 @@ class App extends Component {
             j++;
         }
 
-        console.log(result)
+        this.saveInLocalStorage(this.state.vie, this.state.kor, result.join(""))
 
         this.setState({
             kor: this.state.kor,
@@ -61,6 +85,7 @@ class App extends Component {
 
         const element = document.getElementById("result");
         element.scrollIntoView()
+
 
         // this.copyTextWithStyles(element)
     }
@@ -107,14 +132,90 @@ class App extends Component {
         })
     }
 
+    saveInLocalStorage (vi, ko, result) {
+        let his = this.getLocalStorage()
+
+        his.unshift ({
+            expiredIn: new Date().getTime() + 86400000,
+            createTime: new Date(),
+            data: {
+                vi : vi,
+                ko : ko,
+                result : result
+            }
+        })
+
+        localStorage.setItem("histories", JSON.stringify(his))
+    }
+
+    getLocalStorage () {
+        const now = new Date().getTime();
+        let his = JSON.parse(localStorage.getItem("histories")) || []
+
+        his = his.filter(v => v.expiredIn >= now)
+
+        return his
+    }
+
+    revertVersion = (data) => {
+        this.setState({
+            kor: data.ko,
+            vie: data.vi,
+        })
+        this.closeHistories()
+    }
+
+    openHistories = () => {
+        let his =this.getLocalStorage()
+        let data = his.map((v, i) => {
+            return {
+                key: i,
+                label: new Date(v.createTime).toLocaleString(),
+                children: <div>
+                <Flex justify={'flex-end'}>
+                    <Button className={"revert-btn"} type="primary" shape="round" icon={<RollbackOutlined />} onClick={() => this.revertVersion(v.data)}>Revert</Button>
+                </Flex>
+                    <br/>
+                <Flex vertical={false} justify={'space-between'} gap={10} className={"histories-group"}>
+
+                    <Card>
+                        <div dangerouslySetInnerHTML={{__html: v.data.ko}}/>
+                    </Card>
+                    <Card>
+                        <div dangerouslySetInnerHTML={{__html: v.data.result}}/>
+                    </Card>
+                    <Card>
+                        <div dangerouslySetInnerHTML={{__html: v.data.vi}}/>
+                    </Card>
+                </Flex>
+                </div>
+            }
+        })
+
+        console.log(his)
+
+        this.setState({
+            isHistoriesOpen: true,
+            histories: data
+        })
+    }
+
+    closeHistories = () => {
+        this.setState({
+            isHistoriesOpen: false
+        })
+    }
+
 
     render() {
         return (
             <div className="App">
                 <div className={"div1"}>
                     {/*<button onClick={this.mergeLeft}>Merge Left Column</button>*/}
-                    <button onClick={this.mergeRight}>Merge</button>
-                    <button onClick={this.copyTextWithStyles}
+                    <button onClick={this.mergeRight} className={"btn green-btn"}
+                            disabled={this.state.kor.length === 0 || this.state.vie.length === 0}
+                    >Merge</button>
+                    <button onClick={this.copyTextWithStyles}  className={"btn red-btn"}
                             disabled={this.state.result.length === 0}>{this.state.btnText}</button>
                 </div>
                 <div className="div2">
@@ -159,9 +260,32 @@ class App extends Component {
                     </div>
 
                 </div>
+                <div className="div2" id={"result"}>
 
-                <div dangerouslySetInnerHTML={{__html: this.state.result}} id={"result"}/>
+                { this.state.result.length > 0 ?
 
+                        <Card>
+                            <div dangerouslySetInnerHTML={{__html: this.state.result}} />
+                        </Card>
+
+                    :
+                    <></>
+                }
+                </div>
+
+                <button className={"btn histories-btn float_right_bottom_btn"} onClick={this.openHistories}>Histories</button>
+                <Drawer
+                    title="Histories"
+                    placement="bottom"
+                    maskClosable={true}
+                    closeIcon={false}
+                    onClose={this.closeHistories}
+                    open={this.state.isHistoriesOpen}
+                    height={'90vh'}
+                    key={"bottom"}
+                >
+                    <Collapse accordion  items={this.state.histories} defaultActiveKey={[0]}  />
+                </Drawer>
 
             </div>
         );
